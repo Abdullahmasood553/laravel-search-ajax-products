4df5deeff18c350b38139185157f09cd5f37f8d8
abdullahmasood553 <abdullahmasood553@gmail.com>
2020-10-08 06:06:01 -0700

    added the code

--- public/assets/main.js
+++ public/assets/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     $("#search_product").autocomplete({
         source: '../autocomplete/search_product',
-      });
+    });
       
     $('#get_search_product').click(function() {
         var product = $('#search_product').val();
@@ -10,7 +10,11 @@ $(document).ready(function() {
             type:'GET',
             URL: 'get_product/'+product,
             success:function(data) {
-                alert(data);
+                if(data == 'Product not available') {
+                    $('#product_msg').show();
+                } else {
+                    window.location = 'http://127.0.0.1:8000/'+data;
+                }
             }
         });
     });
